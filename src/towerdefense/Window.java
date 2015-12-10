bb46905afa221d28ca82bf6ff1e0b3d18bd052f2
Dactaxx <dmpribak@gmail.com>
2015-12-10 18:53:50 -0500

    Copied necessary items over

---
+++ src/towerdefense/Window.java
@@ -0,0 +1,28 @@
+package towerdefense;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Window {
+	public static JFrame frame = new JFrame("Tower Defense");
+	public static int width, height;
+	
+	public static void createWindow() {
+		Window.width = Toolkit.getDefaultToolkit().getScreenSize().width;
+		Window.height = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+		frame.setUndecorated(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		TowerMain towermain = new TowerMain();
+		frame.add(towermain);
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		gd.setFullScreenWindow(frame);
+		frame.setVisible(true);
+		
+	}
+	
+}
