ada41f0042a4f238d3a8016b48a32c3a5306a2ee
Dactaxx <dmpribak@gmail.com>
2015-12-12 16:36:01 -0500

    fix

--- src/towers/Turret.java
+++ src/towers/Turret.java
@@ -31,7 +31,7 @@ public class Turret extends Tower {
 	public void render(Graphics2D g2d) {
 		g2d.drawImage(base, (int)(getX() - base.getWidth() / 2), (int)(getY() - base.getHeight() / 2), null);
 		
-		AffineTransform trans = AffineTransform.getRotateInstance(Math.atan2(MouseInfo.getPointerInfo().getLocation().getY(), MouseInfo.getPointerInfo().getLocation().getX()) - Math.atan2(getY(), getX()) + .5 * Math.PI, turret.getWidth() / 2, turret.getHeight() / 2);
+		AffineTransform trans = AffineTransform.getRotateInstance(Math.atan2(MouseInfo.getPointerInfo().getLocation().getY(), MouseInfo.getPointerInfo().getLocation().getX()) - Math.atan2(getY(), getX()), turret.getWidth() / 2, turret.getHeight() / 2);
 		AffineTransformOp op = new AffineTransformOp(trans, AffineTransformOp.TYPE_BILINEAR);
 		
 		g2d.drawImage(op.filter(turret, null), (int)(getX() - turret.getWidth() / 2), (int)(getY() - turret.getHeight() / 2), null);
