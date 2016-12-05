ec981a22959ecc7b61778f2def92f92f3ffa68cc
Dactaxx <dmpribak@gmail.com>
2016-12-05 14:46:00 -0500

    Implemented bare skeleton for GUI

---
+++ src/towerdefense/GUI.java
@@ -0,0 +1,19 @@
+package towerdefense;
+
+import java.awt.Graphics2D;
+
+public class GUI {
+	public GUI() {
+		
+	}
+	
+	public static void tick() {
+		
+	}
+
+	public static void render(Graphics2D g2d) {
+		
+		
+	}
+	
+}
